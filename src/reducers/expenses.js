9ec32418bc2a9f1e71cda2cf8d61989e27616938
Leah Watt <leahwatt@Leahs-MacBook-Pro.local>
2017-11-30 10:59:49 -0500

    saving data on refresh

--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -22,8 +22,12 @@ export default (state = expensesReducerDefaultState, action) => {
           return expense;
         };
       });
+    case 'SET_EXPENSES':
+      return action.expenses;
     default:
       return state;
   }
 };
+
+
 
